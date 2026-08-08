c8a791b24482fe2579e75564587738680cc025a9
GandharvaK312 <gandharvak2021@gmail.com>
2026-08-08 23:12:23 +0530

    adding fractions and format specifiers

---
+++ tprintf.c
@@ -0,0 +1,16 @@
+/* prints ints and float values in various forms*/
+#include <stdio.h>
+
+int main(void){
+
+	int i; float x;
+
+	i = 40; x = 839.21f;
+
+	printf("| %d | %5d | %-5d | %5.3d |\n", i, i, i, i);
+	printf("| %10.3f | %10.3e | %-10g |\n", x, x, x);
+
+	printf("\"Hello!\"\n");
+
+	return 0;
+}

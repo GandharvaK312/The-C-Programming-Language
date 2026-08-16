e93d0e10b3751d149ffb9caa7cd3523fea1f9c95
GandharvaK312 <gandharvak2021@gmail.com>
2026-08-17 05:05:06 +0530

    second_largest sound logic and elegant code

--- 08-second_largest.c
+++ 08-second_largest.c
@@ -3,14 +3,16 @@
 #include <limits.h>
 #include <stdbool.h>
 
-
-int second_largest(int arr[], int n){
+bool second_largest(int arr[], int n, int* result){
 	int s_largest = INT_MIN, largest = INT_MIN;
 
 	for(int i = 0; i < n; i ++){
 		if( arr[i] > largest) { s_largest = largest; largest = arr[i]; }
+		else if( arr[i] < largest && arr[i] > s_largest ) s_largest = arr[i];
 	}
-	return s_largest;
+
+	if( s_largest == INT_MIN ) return 0;
+	else{ *result = s_largest; return 1; }
 }
 
 int main(void){
@@ -21,8 +23,12 @@ int main(void){
 
 	int arr[n];
 	for(int i = 0; i < n; i ++){ scanf("%d", &arr[i]); }
+	
+	int result;
+	int soln = second_largest(arr, n, &result);
 
-	printf("%d\n", second_largest(arr, n));
+	if( soln == 1 ) printf("%d\n", result);
+	else printf("There is no second largest\n");
 	
 	return 0;
 }
